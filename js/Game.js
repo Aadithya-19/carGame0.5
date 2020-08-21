58d3bcaa6b170bd9c4376aaceef72cf165a3921f
Aadithya-19 <svaadithya@gmail.com>
2020-08-21 15:52:57 +0530

    hi

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,40 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountref = await database.ref('playerCount').once("value");
+      if (playerCountref.exists()){
+        playerCount = playerCountref.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
+    }
+  }
+  play (){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100);
+    Player.getplayerinfo();
+    if(allplayers !== undefined){
+      var y = 130;
+      for (var plr in allplayers ){
+        if (plr === 'player' + player.index){
+          fill("red")
+        }
+        else {
+          fill("black");
+        }
+        y = y + 20;
+        textSize(15);
+        text(allplayers[plr].name+ ":" + allplayers[plr].distance, 120, y);
+      }
+    }
+    if(keyDown(UP_ARROW)&&player.index !== null){
+      player.distance = player.distance + 50;
+      player.update();
     }
   }
 }
